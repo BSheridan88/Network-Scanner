8014ac836a8241a806fda9ec4452593a6d76b177
Bode Sheridan <bodesheridan6@gmail.com>
2026-06-27 19:03:10 -0400

    main work

--- Port_Scanner.py
+++ Port_Scanner.py
@@ -2,7 +2,53 @@ from scanner import *
 from utils import *
 
 def main():
+
+    host = HostScanner()
+    port = Port_Scanner
     print()
+    ip = input("Enter your IP: ")
+    valid_ip = validate_ip(ip)
+    if valid_ip == False:
+        print("Invalid IP Format")
+        print("IP Is Either Too Long Or Too Short")
+        eligible = False
+    else:
+        print("valid ip")
+        eligible = True
+
+    if eligible == True:
+        try:
+            print("Network Scanner Dashboard")
+            print("1. Scan Port")
+            print("2. Scan Range of Ports")
+            print("3. Lookup Port")
+            print("4. Exit Dashboard")
+            choice = int(input(""))
+
+            try:
+                if choice == 1:
+                    port = int(input("What Port Would You Like To Scan"))
+                    report = port.scan(ip,port)
+                    print(f'Report for {port}: {report}')
+                elif choice == 2:
+                    pass
+                elif choice == 3:
+                    pass
+                elif choice == 4:
+                    pass
+                else:
+                    print("Invalid Number")
+                    print("Please Select 1-4")
+            except ValueError:
+                print("Invalid Value")
+
+        except TimeoutError:
+            print("The Scan Timed Out")
+        except OSError:
+            print("Network Error Occured")
+
+
+
 
 
 if __name__ == "__main__":
--- scanner.py
+++ scanner.py
@@ -2,13 +2,10 @@ import socket
 class HostScanner:
     def ping(self,ip):
         if 7 <= len(ip) <= 15:
-            print()
+            return True
             #possible ip range
         else:
-            print("Invalid IP Format")
-            print("IP Is Either Too Long Or Too Short")
-
-
+            return False
 class Port_Scanner:
     def __init__(self):
         self.detector = Service_Detector()
@@ -18,6 +15,9 @@ class Port_Scanner:
 
     def scan_range(self,ip,start_port,end_port):
         print()
+        current_port = ""
+        #go through all ips from start to end
+        self.scan(ip,current_port)
 
 class Service_Detector:
     def __init__(self):
